f3afc0f26bc9955b60f5c628570a43186ab74788
Sefiraat <miakis@me.com>
2022-07-26 15:36:22 +0100

    Move MenuItem

--- src/main/java/dev/sefiraat/sefilib/slimefun/itemgroup/MainFlexGroup.java
+++ src/main/java/dev/sefiraat/sefilib/slimefun/itemgroup/MainFlexGroup.java
@@ -14,7 +14,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.ArrayList;
 import java.util.List;
@@ -105,15 +104,15 @@ public class MainFlexGroup extends FlexItemGroup {
         for (int i = 0; i < menuItems.size(); i++) {
             final MenuItem menuItem = menuItems.get(i);
             final int slot = 9 + i;
-            if (menuItem.itemGroup != null) {
-                final ItemGroup itemGroup = menuItem.itemGroup;
+            if (menuItem.getItemGroup() != null) {
+                final ItemGroup itemGroup = menuItem.getItemGroup();
                 menu.replaceExistingItem(slot, itemGroup.getItem(player));
                 menu.addMenuClickHandler(slot, (player1, i1, itemStack1, clickAction) ->
                     openPage(profile, itemGroup, mode, 1)
                 );
             } else if (menuItem.getItem() != null && menuItem.getClickHandler() != null) {
-                menu.replaceExistingItem(slot, menuItem.item);
-                menu.addMenuClickHandler(slot, menuItem.clickHandler);
+                menu.replaceExistingItem(slot, menuItem.getItem());
+                menu.addMenuClickHandler(slot, menuItem.getClickHandler());
             }
         }
     }
@@ -150,90 +149,4 @@ public class MainFlexGroup extends FlexItemGroup {
         return this;
     }
 
-    /**
-     * Represents an icon in the {@link MainFlexGroup}
-     */
-    public static class MenuItem {
-        @Nullable
-        private ItemStack item;
-        @Nullable
-        private ChestMenu.MenuClickHandler clickHandler;
-        @Nullable
-        private ItemGroup itemGroup;
-
-        /**
-         * Creates a new MenuItem
-         *
-         * @param itemGroup The {@link ItemGroup} to be added. Accepts Flex, Nested and ItemGroup (DummyItemGroup)
-         *                  is preferable.
-         */
-        private MenuItem(@Nonnull ItemGroup itemGroup) {
-            this.itemGroup = itemGroup;
-        }
-
-        /**
-         * Creates a new {@link MenuItem} with an {@link ItemStack} and a {@link ChestMenu.MenuClickHandler}
-         *
-         * @param item         The {@link ItemStack} to be displayed
-         * @param clickHandler The {@link ChestMenu.MenuClickHandler} to be executed when the item is clicked
-         */
-        private MenuItem(@Nonnull ItemStack item, @Nonnull ChestMenu.MenuClickHandler clickHandler) {
-            this.item = item;
-            this.clickHandler = clickHandler;
-        }
-
-        /**
-         * Returns the {@link ItemStack} that will be displayed to the players when in the guide.
-         *
-         * @return The {@link ItemStack} to be displayed
-         */
-        @Nullable
-        public ItemStack getItem() {
-            return item;
-        }
-
-        /**
-         * Returns the {@link ChestMenu.MenuClickHandler} for this {@link MenuItem}
-         *
-         * @return The {@link ChestMenu.MenuClickHandler} for this {@link MenuItem}
-         */
-        @Nullable
-        public ChestMenu.MenuClickHandler getClickHandler() {
-            return clickHandler;
-        }
-
-        /**
-         * Returns the {@link ItemGroup} of this {@link MenuItem}
-         *
-         * @return The {@link ItemGroup} of this {@link MenuItem}
-         */
-        @Nullable
-        public ItemGroup getItemGroup() {
-            return itemGroup;
-        }
-
-        /**
-         * Creates a new MenuItem derived from an ItemGroup
-         *
-         * @param itemGroup the ItemGroup to be added to the group
-         * @return Returns a new MenuItem
-         */
-        @Nonnull
-        public static MenuItem of(ItemGroup itemGroup) {
-            return new MenuItem(itemGroup);
-        }
-
-        /**
-         * Creates a new MenuItem derived from an ItemStack and a custom
-         * {@link me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu.MenuClickHandler}
-         *
-         * @param item         the ItemStack to display on the guide
-         * @param clickHandler the MenuClickHandler to be executed when the item is clicked
-         * @return Returns a new MenuItem
-         */
-        @Nonnull
-        public static MenuItem of(ItemStack item, ChestMenu.MenuClickHandler clickHandler) {
-            return new MenuItem(item, clickHandler);
-        }
-    }
 }
